788f799117a0fc7cf0c33cd3be943b3101fe1bd6
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-05-07 00:47:13 +0530

    my first front page

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
+import { IonIcon } from "@ionic/react";
+import { close } from "ionicons/icons";
 import { NavLink } from "react-router-dom";
 
 export default function Todo() {
   const [id, setId] = useState(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [task, setTask] = useState([
+    { title: "What u need to do", content: "Just keep calm & eat 🍕" },
+    {
+      title: "And then ?",
+      content: "After that just create another new task 😘",
+    },
+  ]);
 
   function profileOnClick() {
     document.querySelector("title").innerHTML = "Profile";
@@ -13,29 +24,110 @@ export default function Todo() {
     setId(localStorage.getItem("user-id"));
   }, 1000);
 
-  // if (data) {
+  function addTask(event) {
+    event.preventDefault();
+    let newTask = task;
+    newTask.push({ title: title, content: content });
+    setTask(newTask);
+  }
+
+  function deleteTask(key) {
+    setTask(task.splice(1, key));
+  }
+
   return (
     <div className="homeBody">
       <div className="hero">
-        <h1>Welcome to To-Do Application</h1>
+        <h1>WELCOME 2 TO DO</h1>
+        <p>
+          With a stroke of your keyboard or a gentle tap of your finger,
+          effortlessly manifest your dreams into tasks. Immerse yourself in the
+          magical realm of organization, where tasks are gracefully arranged,
+          waiting to be conquered. Whether you need to complete assignments,
+          plan events, or simply remember the small joys of life, our intuitive
+          interface ensures that every task finds its rightful place
+        </p>
       </div>
 
-      {id == null ? (
-        <NavLink to="/account" onClick={profileOnClick} className="start">
-          <div className="start__add">
-            <div className="a"></div>
-            <div className="b"></div>
+      <div className="heroBody">
+        <div className="start">
+          <p>Click here to transform your dreams into accomplishments</p>
+          {id == null ? (
+            <NavLink
+              to="/account"
+              onClick={profileOnClick}
+              className="startBtn"
+            >
+              <div className="startBtn__add">
+                <div className="a"></div>
+                <div className="b"></div>
+              </div>
+            </NavLink>
+          ) : (
+            <NavLink to="/todo" onClick={profileOnClick} className="startBtn">
+              <div className="startBtn__add">
+                <div className="a"></div>
+                <div className="b"></div>
+              </div>
+            </NavLink>
+          )}
+          <p>
+            Take a deep breath and imagine the possibilities. With our
+            extraordinary Todo application website by your side, you hold the
+            key to unlocking your full potential. Embrace the power of
+            organization, and watch as your dreams gracefully manifest into
+            reality
+          </p>
+        </div>
+
+        <div className="demo">
+          <div className="demoTaskCard demoTaskCard__secondary">
+            <h1>Demo Task</h1>
+
+            {/* Add Task Button */}
+            <div className="addDemoTask addDemoTask__secondary">
+              <form>
+                <div id="pending">
+                  <textarea
+                    rows="2"
+                    type="textarea"
+                    className="inputs1"
+                    placeholder=" Title"
+                    required
+                    onChange={(event) => setTitle(event.target.value)}
+                  ></textarea>
+                  <textarea
+                    rows="3"
+                    type="textarea"
+                    className="inputs2"
+                    placeholder=" Description"
+                    required
+                    onChange={(event) => setContent(event.target.value)}
+                  ></textarea>
+                </div>
+                <button onClick={addTask}>+</button>
+              </form>
+            </div>
+
+            {/* Tasks */}
+            {task.map((element, i) => {
+              return (
+                <div key={i} className="taskItem taskItem__secondary">
+                  <div className="cross">
+                    <div className="crossButton" onClick={() => deleteTask(i)}>
+                      <IonIcon icon={close} />
+                    </div>
+                  </div>
+                  <div className="text">
+                    <h2>{element.title}</h2>
+                    <p>{element.content}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-        </NavLink>
-      ) : (
-        <NavLink to="/todo" onClick={profileOnClick} className="start">
-          <div className="start__add">
-            <div className="a"></div>
-            <div className="b"></div>
-          </div>
-        </NavLink>
-      )}
+        </div>
+      </div>
     </div>
   );
-  // }
 }
--- client/src/profile/Profile.jsx
+++ client/src/profile/Profile.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./Profile.scss";
-import axios from "axios";
+import axios, { all } from "axios";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { IonIcon } from "@ionic/react";
-import { add, close, logOutOutline, createOutline } from "ionicons/icons";
+import {
+  chevronUpOutline,
+  chevronDownOutline,
+  logOutOutline,
+  createOutline,
+} from "ionicons/icons";
 import Todo from "../todolist/Todo";
 
 export default function Profile() {
   const [data, setData] = useState(null);
-
   const [id, setId] = useState(null);
+  const [task, setTask] = useState([]);
+  const [isHidden, setIsHidden] = useState(false);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [c, setC] = useState(0);
 
   const navigate = useNavigate();
 
@@ -31,6 +40,24 @@ export default function Profile() {
       });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/read/${id}`)
+      .then((response) => {
+        let allTask = response.data.data;
+        setTask(allTask);
+
+        let n = allTask[0].length + allTask[1].length + allTask[2].length;
+
+        if (allTask[0].length != 0) setA((allTask[0].length * 100) / n);
+        if (allTask[1].length != 0) setB((allTask[1].length * 100) / n);
+        if (allTask[2].length != 0) setC((allTask[2].length * 100) / n);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [task]);
+
   function edit() {}
 
   function logOut() {
@@ -39,12 +66,30 @@ export default function Profile() {
     navigate("/");
   }
 
-  if(data) 
+  function hide() {
+    if (isHidden) setIsHidden(false);
+    else setIsHidden(true);
+  }
+
+  const statStyle1 = {
+    background: "#F1948A",
+    width: `${a}%`,
+  };
+  const statStyle2 = {
+    background: "#9286fd",
+    width: `${b}%`,
+  };
+  const statStyle3 = {
+    background: "#82E0AA",
+    width: `${c}%`,
+  };
+
+  if (data)
     return (
       <div className="proComponent">
         <div className="componentHeader">
-          <h2 className="proHeading">Profile</h2>
-          <div className="empty"></div>
+          <h2 className="proHeading">{isHidden ? "Task" : "Profile"}</h2>
+          {/* <div className="empty"></div> */}
           {/* <div className="edit" onClick={edit}>
             <IonIcon icon={createOutline} />
           </div> */}
@@ -52,17 +97,39 @@ export default function Profile() {
             <IonIcon icon={logOutOutline} />
           </div>
         </div>
+        {!isHidden && (
+          <>
+            <div className="profile">
+              <div className="profileImage"></div>
+              <div className="profileDetails">
+                <p>{data.name}</p>
+                <p>{data.email}</p>
+              </div>
+              <div className="profileDetails">
+                <p>&nbsp;</p>
+                <p>987654320</p>
+              </div>
+              <div className="profileDetails">
+                <p>&nbsp;</p>
+                <div className="taskStat">
+                  <p>TaskStat</p>
+                  <div style={statStyle1}>&nbsp;</div>
+                  <div style={statStyle2}>&nbsp;</div>
+                  <div style={statStyle3}>&nbsp;</div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
 
-        <div className="profile profile__secondary">
-          <h2>
-            <b>Name</b> {data.name}
-          </h2>
-          <h3>
-            <b>Email</b> {data.email}
-          </h3>
+        <div className="up_down" onClick={hide}>
+          {isHidden ? (
+            <IonIcon icon={chevronDownOutline} />
+          ) : (
+            <IonIcon icon={chevronUpOutline} />
+          )}
         </div>
-          <Todo />
+        <Todo />
       </div>
     );
-  
 }
